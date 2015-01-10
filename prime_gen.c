1898776bd80220e591e46c2828094c7bb7001a19
Cukta <Cukta@users.noreply.github.com>
2015-01-10 09:47:11 -0600

    Create prime_gen.c

---
+++ prime_gen.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "vector.h"
+
+#define TRUE 1
+#define FALSE 0
+
+void main(){
+	Vector primes;
+	vector_init(&primes);
+
+	int num_primes;
+	printf("How many primes do you want to generate: \t");
+	scanf_s("%d", &num_primes);
+
+	int current_prime;
+
+	int i;
+	for (i = 0; i < num_primes; ++i){
+		int is_prime = TRUE;
+		if (i == 0)
+			current_prime = 2;
+		else if (i == 1){
+			current_prime = 3;
+		}
+		else{
+			int j;
+			current_prime += 2;
+			for (j = 0; j < primes.size; ++j){
+				if (current_prime % primes.data[j] == 0){
+					is_prime = FALSE;
+					break;
+				}
+				if (current_prime < primes.data[j] * primes.data[j])
+					break;
+			}
+		}
+		if (is_prime)
+			vector_append(&primes, current_prime);
+	}
+
+	for (i = 0; i < primes.size; ++i){
+		printf("%d\n", primes.data[i]);
+	}
+
+	vector_free(&primes);
+
+	system("PAUSE");
+}
